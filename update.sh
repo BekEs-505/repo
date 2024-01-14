07f67a020447212240dc369e15afa874f770f6f8
System Administrator <BekEs-505>
2024-01-14 19:26:11 +0300

    first commit

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dpkg-scanpackages -m ./debs > Packages
-rm -r -f /Users/BekEs-505/Documents/GitHub/repo/Packages.bz2
+rm -r -f /var/mobile/repo/Packages.bz2
 bzip2 Packages
 dpkg-scanpackages -m ./debs > Packages
